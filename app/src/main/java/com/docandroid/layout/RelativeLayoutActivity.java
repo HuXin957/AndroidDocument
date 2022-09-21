294b6cae4d48baf81983aa6f447a28b338036749
majin5555 <13051152997@163.com>
2022-09-21 11:09:18 +0800

    相对布局

--- app/src/main/java/com/docandroid/layout/RelativeLayoutActivity.java
+++ app/src/main/java/com/docandroid/layout/RelativeLayoutActivity.java
@@ -2,16 +2,25 @@ package com.docandroid.layout;
 
 import android.annotation.SuppressLint;
 import android.graphics.Color;
+import android.widget.TextView;
 
 import com.baseframework.BaseActivity;
 import com.baseframework.interfaces.Layout;
 import com.baseframework.util.JumpParameter;
+import com.docandroid.ConstantParams;
 import com.docandroid.R;
 
 
+/**
+ * @author: majin
+ * @createTime: 2022/9/21
+ * @desc: 相对布局
+ */
 @SuppressLint("NonConstantResourceId")
 @Layout(R.layout.activity_relative_layout)
 public class RelativeLayoutActivity extends BaseActivity {
+    private TextView mTitle;
+
 
     @SuppressLint("ResourceType")
     @Override
@@ -21,12 +30,12 @@ public class RelativeLayoutActivity extends BaseActivity {
     }
     @Override
     public void initViews() {
-
+        mTitle = findViewById(com.baseframework.R.id.txt_title);
     }
 
     @Override
     public void initDatas(JumpParameter parameter) {
-
+        mTitle.setText(parameter.getString(ConstantParams.LAYOUTNAME));
     }
 
     @Override
